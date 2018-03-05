2654c12b029712e222536cbe6ffbe8082e1f50be
Jbin <kjb7907@naver.com>
2018-03-05 17:47:30 +0900

    no message

--- pages/solutions.vue
+++ pages/solutions.vue
@@ -1,6 +1,9 @@
 <template>
 <div>
   <solutions-main></solutions-main>
+  <div class="container">
+    <div  style="width:100%; height:600px;"><h1>solution description area</h1></div>
+  </div>
 </div>
 </template>
 
